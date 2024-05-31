7d7414b139a84590d75edf018bda3f017c3f5f90
IrisKerr <iris.kerr@hotmail.com>
2024-05-31 17:09:26 +0200

    ✨feat: add Navbar + fix typo

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="main-nav">
+    <router-link :to="{ name: 'Home' }">Projects</router-link>
+    <router-link :to="{ name: 'AddProject' }">Add a new Project</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Navbar",
+};
+</script>
+
+<style></style>
